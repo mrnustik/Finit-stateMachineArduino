c43002f71537be4dcb078872f149a1c6c2cb7501
Michal Mrnustik <michal.mrnustik@outlook.com>
2014-12-30 17:05:47 +0100

    Added machine states table.

---
+++ FinitStateMachine.ino
@@ -0,0 +1,60 @@
+
+#include <SoftwareSerial.h>
+
+
+typedef struct{
+	String		attributeName;
+	String		attributeValue
+
+} Attribute;
+
+typedef struct
+{
+	String		elementName;
+	Attribute*	attributes;
+	int			attributeCount;
+
+} Command;
+
+
+
+SoftwareSerial	Bluetooth(3, 4); //RX, TX
+Command			lastCommand;
+
+
+#define Debug	Serial
+#define DEBUG	1
+
+
+void debugLog(String message){
+	if (DEBUG) {
+		Debug.println("DEBUG: " + message);
+	}
+}
+
+
+void setup()
+{
+	Bluetooth.begin(9600);
+
+	Debug.begin(57600);
+	while (!Debug.available()){
+	}
+	
+	debugLog("The board has started.");
+}
+
+
+void loop()
+{
+	char lastReadChar;
+
+	if (Bluetooth.available()){
+		debugLog("Bluetooth connected");
+
+		lastReadChar = Bluetooth.read();
+
+
+	}
+
+}
