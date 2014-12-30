50b63c6c7451b63da3ef3d65b1779eb06e38ddd8
Michal Mrnustik <michal.mrnustik@outlook.com>
2014-12-30 17:23:03 +0100

    Added states and states table.

--- FinitStateMachine.ino
+++ FinitStateMachine.ino
@@ -18,13 +18,24 @@ typedef struct
 
 
 
+int				statesTable[6][5] = { { 2, 6, 6, 6, 6 }, { 6, 2, 3, 6, 5 }, { 6, 3, 6, 4, 6 }, { 6, 4, 3, 6, 5 }, { 1, 1, 1, 1, 1 }, { 6, 6, 6, 6, 6 } };
+
+
+
 SoftwareSerial	Bluetooth(3, 4); //RX, TX
 Command			lastCommand;
-
+int				currentState;
 
 #define Debug	Serial
 #define DEBUG	true
 
+
+#define WAITING_STATE				1
+#define READ_ELEMENT_NAME_STATE		2
+#define READ_ATTRIBUTE_NAME_STATE	3
+#define READ_ATTRIBUTE_VALUE_STATE	4
+#define HANDLE_DATA_STATE			5
+#define ERROR_STATE					6
 
 void debugLog(String message){
 	if (DEBUG) {
